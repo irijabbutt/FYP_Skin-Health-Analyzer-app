3d4e39de9b2c20ed3466f0654d9b811f2b31481e
irijabbutt <rijabbutt23@gmail.com>
2026-07-02 16:16:33 +0500

    remaining files

--- skinanalyzer/lib/main.dart
+++ skinanalyzer/lib/main.dart
@@ -5,6 +5,8 @@
 // Description: App entry point with Supabase & TFLite init
 // -----------------------------------------------
 
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
